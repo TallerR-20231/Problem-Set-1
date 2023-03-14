07011d1f16520f5a92eb993ddd9c8f054d1f1a47
sagujara <s.gutierrezj@uniandes.edu.co>
2023-03-13 21:20:47 -0500

    Punto 2 realizado:

--- source/code_problem_set1.R
+++ source/code_problem_set1.R
@@ -2,7 +2,9 @@
 ##update: 3/6/2023, 13:40
 ##Integrantes:
   #Nicolas Lozano - 202021428
-  #Samuel Gutierrez - 
+  #Samuel Gutierrez - 202111674
+
+## Versión de r: 4.2.2
 
 #Limpiar todo
 rm(list = ls())
@@ -22,11 +24,20 @@ p_load(dplyr,
        janitor) 
 
 
-#1. Vectores
+#1. Vectores:
 num_uno_cien = 1:100
 bool_impar <- num_uno_cien%%2!=0
 num_impares <- num_uno_cien[bool_impar]
 bool_par <- !(num_uno_cien %in% num_impares)
 num_pares <- num_uno_cien[bool_par]
 
-#2. 
+#2. Importar/exportar bases de datos:
+##Encuesta micronegocios DANE:
+
+#2.1. Importar:
+mi <- import('input/Módulo de identificación.dta')
+ms <- import('input/Módulo de sitio o ubicación.dta')
+
+#2.2. Exportar:
+export(mi, "output/Modulo_identificación.rds")
+export(ms, "output/Modulo_ubicación.rds")
